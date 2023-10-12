43554da608ae915ca504575c798a922463b7eee9
oloapinivad <p.davini@isac.cnr.it>
2023-10-12 10:51:29 +0000

    notebook temp check

--- martini/nemo-restart.py
+++ martini/nemo-restart.py
@@ -13,7 +13,8 @@ import argparse
 import xarray as xr
 
 # on atos, you will need to have 
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/apps/netcdf4/4.9.1/INTEL/2021.4/lib:/usr/local/apps/hdf5/1.12.2/INTEL/2021.4/lib
+# module load intel 
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/apps/netcdf4/4.9.1/INTEL/2021.4/lib:/usr/local/apps/hdf5/1.12.2/INTEL/2021.4/lib
 
 def parse_args():
     """Command line parser for nemo-restart"""
@@ -21,7 +22,10 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Command Line Parser for nemo-restart")
 
     # add positional argument (mandatory)
-    parser.add_argument("expname",  metavar="EXPNAME", help="experiment name")
+    parser.add_argument("expname", metavar="EXPNAME", help="Experiment name")
+    parser.add_argument("leg", metavar="LEG", help="The leg you want to process for rebuilding", type=str)
+
+    # optional to activate nemo rebuild
     parser.add_argument("--rebuild", action="store_true", help="Enable nemo-rebuild")
 
     args = parser.parse_args()
@@ -35,7 +39,7 @@ def get_nemo_timestep(DIR, nemofilename):
 
     return timestep
 
-def rebuild_nemo(expname, REBUILD_DIR, TMP_DIR):
+def rebuild_nemo(expname, leg, REBUILD_DIR, TMP_DIR):
 
     EXP_DIR = os.path.join(BASE_DIR, expname)
     rebuilder = os.path.join(REBUILD_DIR, "rebuild_nemo")
@@ -43,7 +47,7 @@ def rebuild_nemo(expname, REBUILD_DIR, TMP_DIR):
     for kind in ['restart', 'restart_ice']: 
         print('Processing' + kind)
         #timestep = get_nemo_timestep(os.path.join(EXP_DIR, 'restart', '002'), expname + '*_' + kind + '_????.nc')
-        filelist = glob.glob(os.path.join(EXP_DIR, 'restart', '002', expname + '*_' + kind + '_????.nc'))
+        filelist = glob.glob(os.path.join(EXP_DIR, 'restart', leg.zfill(3), expname + '*_' + kind + '_????.nc'))
         timestep = os.path.basename(filelist[0]).split('_')[1]
 
         for filename in filelist:
@@ -69,16 +73,16 @@ if __name__ == "__main__":
     
     # parser
     args = parse_args()
+    expname = args.expname
+    leg = args.leg
 
     BASE_DIR="/ec/res4/scratch/itas/ece4"
-    TMP_DIR = "/lus/h2resw01/scratch/ccpd/martini"
-    REBUILD_DIR="/ec/res4/scratch/itas/ece4/res1/restart/002"
+    TMP_DIR = os.path.join("/lus/h2resw01/scratch/ccpd/martini", expname, leg.zfill(3))
+    REBUILD_DIR="/ec/res4/hpcperm/itas/src/rebuild_nemo"
     os.makedirs(TMP_DIR, exist_ok=True)
 
-    expname = args.expname
-
     if args.rebuild:
-        rebuild_nemo(expname=expname, REBUILD_DIR=REBUILD_DIR, TMP_DIR=TMP_DIR)
+        rebuild_nemo(expname=expname, leg=leg, REBUILD_DIR=REBUILD_DIR, TMP_DIR=TMP_DIR)
 
     timestep = get_nemo_timestep(TMP_DIR,  expname + '*_restart.nc')
     oce = os.path.join(TMP_DIR, expname + '_' + timestep + '_restart.nc')
