16176aff625e641566ab7146212dfd3172bd66df
oloapinivad <p.davini@isac.cnr.it>
2023-10-12 13:37:09 +0000

    cleaning and new environment

--- martini/functions.py
+++ martini/functions.py
@@ -1,4 +1,5 @@
 def preproc_nemo(field):
+    """preprocessing routine for nemo"""
 
     field = field.rename_dims({'x_grid_T': 'x', 'y_grid_T': 'y', 'time_counter': 'time'})
     field = field.rename({'time_counter': 'time'})
