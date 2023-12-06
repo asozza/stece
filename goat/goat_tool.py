7c18dbebb0a22fc77363c96b3fde27ecff43436e
Alessandro Sozza <itas@ac6-102.bullx>
2023-12-06 13:45:43 +0000

    update goat

---
+++ goat/goat_tool.py
@@ -0,0 +1,99 @@
+
+import numpy as np
+import pandas as pd
+import datetime
+import time
+from sklearn.linear_model import LinearRegression
+
+def preproc_nemo_T(data):
+    """preprocessing routine for nemo for T grid"""
+
+    data = data.rename_dims({'x_grid_T': 'x', 'y_grid_T': 'y'})
+    data = data.rename({'deptht': 'z', 'time_counter': 'time', 'thetao': 'to'})
+    data = data.swap_dims({'x_grid_T_inner': 'x', 'y_grid_T_inner': 'y'})
+    data.coords['z'] = -data['z']
+    
+    return data
+
+def preproc_nemo_U(data):
+    """preprocessing routine for nemo for U grid"""
+
+    data = data.rename_dims({'x_grid_U': 'x', 'y_grid_U': 'y'})
+    data = data.rename({'depthu': 'z', 'time_counter': 'time'})
+    data = data.swap_dims({'x_grid_U_inner': 'x', 'y_grid_U_inner': 'y'})
+    data.coords['z'] = -data['z']
+    
+    return Tdata
+
+def preproc_nemo_V(data):
+    """preprocessing routine for nemo for V grid"""
+
+    data = data.rename_dims({'x_grid_V': 'x', 'y_grid_V': 'y'})
+    data = data.rename({'depthv': 'z', 'time_counter': 'time'})
+    data = data.swap_dims({'x_grid_V_inner': 'x', 'y_grid_V_inner': 'y'})
+    data.coords['z'] = -data['z']
+    
+    return data
+
+def preproc_nemo_W(data):
+    """preprocessing routine for nemo for W grid"""
+
+    data = data.swap_dims({'x_grid_W_inner': 'x', 'y_grid_W_inner': 'y'})
+    data = data.rename_dims({'x_grid_W': 'x', 'y_grid_W': 'y'})
+    data = data.rename({'depthw': 'z', 'time_counter': 'time'})
+    data.coords['z'] = -data['z']
+
+    return data
+
+def preproc_nemo_ice(data):
+    """preprocessing routine for nemo for ice"""
+
+    data = data.rename({'deptht': 'z', 'time_counter': 'time'})
+    data.coords['z'] = -data['z']
+    
+    return data
+
+def epoch(date):
+
+    s = time.mktime(date.timetuple())
+
+    return s
+
+def yearFraction(date):
+
+    StartOfYear = datetime.datetime(date.year,1,1,0,0,0)
+    EndOfYear = datetime.datetime(date.year+1,1,1,0,0,0)
+    yearElapsed = epoch(date)-epoch(StartOfYear)
+    yearDuration = epoch(EndOfYear)-epoch(StartOfYear)
+    Frac = yearElapsed/yearDuration
+
+    return  date.year + Frac
+
+def dateDecimal(datatime):
+
+    d1 = pd.to_datetime(datatime)
+    x1 = [yearFraction(t) for t in d1]
+
+    return x1
+
+def interp_average(xdata, ydata, N):
+
+    x_orig = np.array(dateDecimal(xdata.values))
+
+    for i in range(N):    
+        x_filled = np.array(dateDecimal(xdata.where(xdata['time.month']==i+1,drop=True).values))
+        y_filled = np.array(ydata.where(xdata['time.month']==i+1,drop=True).values.flatten())
+        if (i==0):
+            y_smooth = np.interp(x_orig, x_filled, y_filled)/N
+        else:
+            y_smooth += np.interp(x_orig, x_filled, y_filled)/N
+    
+    return y_smooth
+
+def moving_average(ydata, N):
+
+    #y_list = np.array(ydata.values.flatten())
+    y_padded = np.pad(ydata, (N//2, N-1-N//2), mode='edge')
+    y_smooth = np.convolve(y_padded, np.ones((N,))/N, mode='valid')
+
+    return y_smooth
