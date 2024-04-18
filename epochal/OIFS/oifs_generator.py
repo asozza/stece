6a527f5fbc06239985d5331cdf0ea136a5457e1c
oloapinivad <p.davini@isac.cnr.it>
2024-04-18 11:05:41 +0000

    working with remapnn

--- epochal/OIFS/oifs_generator.py
+++ epochal/OIFS/oifs_generator.py
@@ -14,8 +14,8 @@ Paolo Davini (CNR-ISAC, Apr 2024)
 import subprocess
 import os
 import shutil
-from utils import extract_grid_info, ecmwf_grid
 import cdo
+from utils import extract_grid_info, ecmwf_grid
 cdo = cdo.Cdo()
 cdo.debug = True
 
@@ -69,24 +69,23 @@ else:
 
 # INITIAL CONDITIONS
 print("Truncating spectral file ICMSHECE4INIT to", spectral, "harmonics")
-# this is done with a clean spectral truncation with cdo
+# This is done with a clean spectral truncation with cdo. 
+# Orography is therefore realiable.
 OIFS_IC = os.path.join(OIFS_BASE, source_grid, startdate)
-cdo.sp2sp(spectral, option="--eccodes", input=f"{OIFS_IC}/ICMSHECE4INIT", 
+cdo.sp2sp(spectral, option="--eccodes", input=f"{OIFS_IC}/ICMSHECE4INIT",
           output=f"{TMPDIR}/ICMSHECE4INIT")
 
 
 print ("Remapping ICMGG gaussian ICs to", target_grid)
-# this is done with remapcon using the grid fils computed with oifs_create_corner.py
-#gridfile = os.path.join('grids', horizontal_grid + '.txt')
+# This is done with remapcon using the grid fils computed with oifs_create_corner.py
 for file in ["ICMGGECE4INIT", "ICMGGECE4INIUA"]:
-    cdo.remapcon(f"{GRIDS}/{target_spectral}_grid.nc",
-                 input=f"-setgrid,{GRIDS}/{source_spectral}_grid.nc {OIFS_IC}/{file}",
-                 output=f"{TMPDIR}/{file}.tmp")
-    cdo.setgrid(f"grids/{target_spectral}.txt", input=f"{TMPDIR}/{file}.tmp", output=f"{TMPDIR}/{file}")
-    os.remove(f"{TMPDIR}/{file}.tmp")
+    #icmtmp = cdo.remapcon(f"{GRIDS}/{target_spectral}_grid.nc",
+    #             input=f"-setgrid,{GRIDS}/{source_spectral}_grid.nc {OIFS_IC}/{file}")
+    #cdo.setgrid(f"grids/{target_spectral}.txt", input=icmtmp, output=f"{TMPDIR}/{file}")
+    cdo.remapnn(f"grids/{target_spectral}.txt", input=f"{OIFS_IC}/{file}", output=f"{TMPDIR}/{file}")
     
 # BOUNDARY CONDITIONS
-# this is done with a mergetime of the 7 variables in the ecmwf directory based on a magic command by Klaus Wyser
+# This is done with a mergetime of the 7 variables in the ECMWF directory based on a magic command by Klaus Wyser
 print("Building BCs from", ecmwf_name, "data")
 variables = ["alb", "aluvp", "aluvd", "alnip", "alnid", "lail", "laih"]
 paths = [f"{OIFS_BC}/{ecmwf_name}/month_{var}" for var in variables]
@@ -104,7 +103,14 @@ if not do_vertical:
     for file in ["ICMSHECE4INIT", "ICMGGECE4INIT", "ICMGGECE4INIUA"]:
         shutil.move(f"{TMPDIR}/{file}", f"{IC_TGT}/{file}")
 
-# go for hybrid levels interpolation
+# Procedure for vertical interpolation requires all the data to be in grid point space.
+# This is done by converting the spectral fields to gaussian grids and then moving back them to the spectral space 
+# It has been decided to interpolate spectral data (T, D, V) and keep gaussian data (Q, etc.) on the gaussian reduced grid
+# Orography and surface pressure are not touched and attached to the files at the end of the operations
+# A-B coefficients for remapeta are downloaded from ECMWF website and then converted to txt file 
+# in CDO-compliant style with convert_aka_bika.py script. These are stored in the grids folder. 
+# To set gaussian reduced grids the grid files are produced with descriptor_generator.py and
+# also stored in txt file in the grids folder
 else:
     print("Vertical interpolation is necessary")
     print("Select z and lnsp from ICMSHECE4INIT...")
@@ -120,7 +126,8 @@ else:
 
     # We then bring them on the same gaussian reduced grid
     print("Remapping spectral fields from gaussian to gaussian reduced")
-    cdo.remapcon(f"{GRIDS}/{target_spectral}_grid.nc", input=f"{TMPDIR}/sp2gauss.grb", output=f"{TMPDIR}/sp2gauss_reduced.grb")
+    remapped = cdo.remapcon(f"{GRIDS}/{target_spectral}_grid.nc", input=f"{TMPDIR}/sp2gauss.grb")
+    cdo.setgrid(f"grids/{target_spectral}.txt", input=remapped, output=f"{TMPDIR}/sp2gauss_reduced.grb")
 
     # Merge files to prepare for interpolation
     print("Merging files")
@@ -132,22 +139,22 @@ else:
     cdo.remapeta(gridfile, input=f"{TMPDIR}/single.grb", output=f"{TMPDIR}/remapped.grb")
 
     # create INITUA file
-    print("Selecting fields to create ICMSHECE4INIUA")
-    cdo.setgrid(f"grids/{target_spectral}.txt", input=f"-selname,q,o3,crwc,cswc,clwc,ciwc,cc {TMPDIR}/remapped.grb", output=f"{TMPDIR}/ICMGGECE4INIUA")
+    print("Selecting fields to create ICMSHECE4INIUA and setting gaussian reduced grid")
+    cdo.setgrid(f"grids/{target_spectral}.txt", input=f"-selname,q,o3,crwc,cswc,clwc,ciwc,cc {TMPDIR}/remapped.grb", output=f"{IC_TGT}/ICMGGECE4INIUA")
 
     # Nring new field to spectral space
-    print("Converting back to spectral the spectral fields")
-    cdo.gp2spl(input=f"-selname,t,vo,d {TMPDIR}/remapped.grb", output=f"{TMPDIR}/spback.grb")
+    print("Converting back to spectral (through gaussian regular) the spectral fields")
+    cdo.gp2spl(input=f"-setgridtype,regular -selname,t,vo,d {TMPDIR}/remapped.grb", output=f"{TMPDIR}/spback.grb")
 
     # Merge with orography and lnsp and get the SH file
     print("Merging files and creating the final ICMSHECE4INIT")
-    subprocess.call(f"cat {TMPDIR}/lnsp2.grb {TMPDIR}/spback.grb {TMPDIR}/orog.grb > {IC_TGT}/ICMSHECE4INIT", shell=True)
+    subprocess.call(f"cat {TMPDIR}/spback.grb {TMPDIR}/lnsp2.grb  {TMPDIR}/orog.grb > {IC_TGT}/ICMSHECE4INIT", shell=True)
 
     shutil.move(f"{TMPDIR}/ICMGGECE4INIT", f"{IC_TGT}/ICMGGECE4INIT")
 
     if do_clean:
         print("Cleaning up")
-        for file in ["gp2gauss.grb", "sp2gauss.grb", "sp2gauss_reduced.grb" "single.grb", "remapped.grb", 
+        for file in ["gp2gauss.grb", "sp2gauss.grb", "sp2gauss_reduced.grb" "single.grb", "remapped.grb",
                     "ICMSHECE4INIT", "ICMGGECE4INIUA",
                     "remapped.grb", "spback.grb", "lnsp.grb", "lnsp2.grb", "orog.grb"]:
             os.remove(file)
