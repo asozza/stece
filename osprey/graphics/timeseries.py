703465cca845008591c436ca966b71b5fef4c0c8
asozza <asozza.ph@gmail.com>
2024-09-09 09:22:45 +0000

    fix two-experiment timeseries distance

--- osprey/graphics/timeseries.py
+++ osprey/graphics/timeseries.py
@@ -94,10 +94,10 @@ def timeseries(expname,
     return pp
 
 
-def timeseries_diff(expname1, expname2, 
+def timeseries_two(expname1, expname2, 
                     startyear, endyear, 
                     varlabel, 
-                    offset=0, 
+                    timeoff=0, 
                     color=None,
                     rescaled=False,
                     reader="nemo",
@@ -105,13 +105,13 @@ def timeseries_diff(expname1, expname2,
                     metric="base", 
                     avetype="moving"): 
     """ 
-    Graphics of two-experiment difference timeseries 
+    Graphics of two-experiment timeseries distance based on a metric
     
     Args:
     expname1,2: experiment name
     startyear,endyear: time window
     varlabel: variable label (varname + subregion)    
-    offset: time offset
+    timeoff: time offset
     color: curve color
     rescaled: rescale timeseries by the initial value at time=0
     reader_type: read the original raw data or averaged data [nemo, post]
@@ -132,40 +132,39 @@ def timeseries_diff(expname1, expname2,
         data1 = reader_nemo(expname1, startyear, endyear)
         data2 = reader_nemo(expname2, startyear, endyear)
         tvec = get_decimal_year(data1['time'].values)
-    elif reader == 'averaged':
-        data1 = postreader_averaged(expname=expname1, startyear=startyear, endyear=endyear, varlabel=varlabel, diagname='timeseries', replace=replace, metric=metric)
-        data2 = postreader_averaged(expname=expname2, startyear=startyear, endyear=endyear, varlabel=varlabel, diagname='timeseries', replace=replace, metric=metric)
+    elif reader == 'post':
+        data1 = postreader_averaged(expname=expname1, startyear=startyear, endyear=endyear, varlabel=varlabel, diagname='timeseries', replace=replace, metric='base')
+        data2 = postreader_averaged(expname=expname2, startyear=startyear, endyear=endyear, varlabel=varlabel, diagname='timeseries', replace=replace, metric='base')
         tvec = data1['time'].values.flatten()
 
     # fix time axis
     tvec_cutted = tvec[6:-6]
-    tvec_offset = [tvec_cutted[i]+offset for i in range(len(tvec_cutted))]
+    tvec_offset = [tvec_cutted[i]+timeoff for i in range(len(tvec_cutted))]
 
     # fix y-axis
-    vec1 = data1[varname].values.flatten()
-    vec2 = data1[varname].values.flatten()
     if avetype == 'moving':
-        if reader == 'nemo':
-            ndim = vardict('nemo')[varname]
+        if reader == 'nemo':           
             vec1 = movave(spacemean(data1, varname, info['dim'], ztag),12)
             vec2 = movave(spacemean(data2, varname, info['dim'], ztag),12)
         elif reader == 'post':
             vec1 = movave(data1[varlabel],12)
             vec2 = movave(data2[varlabel],12)            
-    vec1_cutted = vec1[6:-6]
-    vec2_cutted = vec2[6:-6]
-    vec_cost = cost(vec1_cutted, vec2_cutted, metric)
+    elif avetype == 'standard':
+        vec1 = data1[varname].values.flatten()
+        vec2 = data1[varname].values.flatten()
+    vec_cost = cost(vec1, vec2, metric)
+    vec_cutted = vec_cost[6:-6]
 
     # apply rescaling
     if rescaled == True:
-        vec_cost = vec_cost/vec_cost[0]
+        vec_cutted = vec_cutted/vec_cutted[0]
 
     # plot
     plot_kwargs = {}
     if color is not None:
         plot_kwargs['color'] = color
     
-    pp = plt.plot(tvec_offset, vec_cost, **plot_kwargs)
+    pp = plt.plot(tvec_offset, vec_cutted, **plot_kwargs)
     plt.xlabel('time')
     plt.ylabel(info['long_name'])
 
